2f5c51b287fe6a60f69e703619b2674992eeb690
beuleu <billyklotz789@gmail.com>
2023-12-02 16:06:57 +0100

    extract isOutOfRange

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,18 +3,27 @@ package games;
 public class FizzBuzz {
 
   public static String convert(Integer input) throws OutOfRangeException {
-    if (input <= 0 || input > 100) {
-      throw new OutOfRangeException();
-    }
+    
+    isOutOfRange(input);
+
     if (input % 3 == 0 && input % 5 == 0) {
       return "FizzBuzz";
     }
+
     if (input % 3 == 0) {
       return "Fizz";
     }
+
     if (input % 5 == 0) {
       return "Buzz";
     }
+
     return input.toString();
+  }
+
+  private static void isOutOfRange(Integer input) throws OutOfRangeException {
+    if (input <= 0 || input > 100) {
+      throw new OutOfRangeException();
+    }
   }
 }
