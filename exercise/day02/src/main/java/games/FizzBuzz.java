31e68c2921db84b8a68fe1fceddc801de32c137d
beuleu <billyklotz789@gmail.com>
2023-12-02 16:48:36 +0100

    final answer

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,8 +3,9 @@ package games;
 public class FizzBuzz {
 
   public static String convert(Integer input) throws OutOfRangeException {
-    
-    isOutOfRange(input);
+    if (input <= 0 || input > 100) {
+      throw new OutOfRangeException();
+    }
 
     if (input % 3 == 0 && input % 5 == 0) {
       return "FizzBuzz";
@@ -19,11 +20,5 @@ public class FizzBuzz {
     }
 
     return input.toString();
-  }
-
-  private static void isOutOfRange(Integer input) throws OutOfRangeException {
-    if (input <= 0 || input > 100) {
-      throw new OutOfRangeException();
-    }
   }
 }
